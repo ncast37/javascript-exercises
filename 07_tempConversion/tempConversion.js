ca141cc6ff66ea6b65354207a4111d9be2a8d45f
ncast37 <nick.castillo37@gmail.com>
2023-07-23 16:19:04 -0800

    Complete conversion functions and pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const tempC = 5/9 * (tempF - 32);
+  return Math.round(tempC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(tempC) {
+  const tempF = (tempC * 9/5) + 32;
+  return Math.round(tempF * 10) / 10;
+
 };
 
 // Do not edit below this line
