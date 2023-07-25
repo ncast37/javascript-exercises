391faa7f52951e4dffabcbdd2dd8a039c362a6b8
ncast37 <nick.castillo37@gmail.com>
2023-07-25 11:58:07 -0800

    Complete the palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    // Remove all non-alphanumeric characters and white space;
+    let strNoPunctuation = string.replace(/[.,!()';:]/g, '');
+    let strNoWhiteSpace = strNoPunctuation.replace(/\s/g,'');
+
+
+    let cleanString = strNoWhiteSpace.toLowerCase();
+    const cleanArray = cleanString.split('');
+    const reverseArray = [...cleanArray].reverse();
+    const length = cleanArray.length;
+
+    let sameBool = true;
+    for (let i = 0; i < length; i++){
+        if (cleanArray[i] === reverseArray[i]){
+            continue;
+        }
+        else {
+            sameBool = false;
+            break;
+        }
+    }
+
+    return sameBool;
 
 };
 
