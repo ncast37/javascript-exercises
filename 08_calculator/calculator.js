dab45c6f1607b112bc24b219dc07b63b8474f6b0
ncast37 <nick.castillo37@gmail.com>
2023-07-24 18:49:50 -0800

    completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,50 @@
-const add = function() {
-	
+const add = function(...values) {
+	let sum = 0;
+  for (let val in values){
+    sum += values[val];
+  }
+
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(...values) {
+	let difference = values[0];
+
+  for (let val in values) {
+    difference -= values[val];
+  }
+
+  difference += values[0];
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum_num = 0;
+  let newArray = [...array];
+
+  for (let val in newArray){
+
+    sum_num += Number(newArray[val]);
+  }
+
+  return sum_num;
 };
 
-const multiply = function() {
+const multiply = function(valuesArray) {
+  let newArray = [...valuesArray];
+  let product = 1;
 
+  for (let val in newArray) {
+    product *= newArray[val];
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+  
+  return (number ** exponent);
+  
 };
 
 const factorial = function() {
