b80a1cf0ef3f994af86252edd6f5507693e656ff
ncast37 <nick.castillo37@gmail.com>
2023-07-25 12:28:59 -0800

    Complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    //Don't allow negative numbers 
+    if (n < 0){
+        return "OOPS";
+    }
+
+    //Convert to integers
+    const num = parseInt(n);
+    let fibSeq = [0, 1];
+
+    let fibNum;
+    let i = 1; 
+    while (i != num) {
+        fibNum = fibSeq[i] + fibSeq[i - 1];
+        fibSeq.push(fibNum)
+        i++; 
+    }
+
+    return fibSeq[num];
 
 };
 
