432e305645d9912e440e5517f964ae11bd41d5e4
ncast37 <nick.castillo37@gmail.com>
2023-07-25 14:45:03 -0800

    Complete the findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest = array.reduce((accumulator, current) => {
+        let currentAge;
+        let accumuatorAge; 
+        const date = new Date();
+        let thisYear = date.getFullYear();
 
+        if (current.hasOwnProperty('yearOfDeath')){
+            currentAge = current.yearOfDeath - current.yearOfBirth;
+        } else {
+            currentAge = thisYear - current.yearOfBirth;
+        }
+
+        if (accumulator.hasOwnProperty('yearOfDeath')){
+            accumuatorAge = accumulator.yearOfDeath - accumulator.yearOfBirth;
+        } else {
+            accumuatorAge = thisYear - accumulator.yearOfBirth;
+        }
+
+        if (currentAge > accumuatorAge){
+            accumulator = current;
+        }
+
+        return accumulator;
+    }
+    ) 
+
+    return oldest;
 };
 
 // Do not edit below this line
